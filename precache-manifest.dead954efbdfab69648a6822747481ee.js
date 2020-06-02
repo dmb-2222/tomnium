3562031678ef21fca505c47e525ef9f45ee8a5b3
dmb-2222 <dmb-2222@yandex.ru>
2020-06-02 19:39:12 +0300

    Updates

---
+++ precache-manifest.dead954efbdfab69648a6822747481ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5141134abd0c25675f0410cb19562e2e",
+    "url": "/tomnium/index.html"
+  },
+  {
+    "revision": "9df644db64623cb23243",
+    "url": "/tomnium/static/css/main.04633120.chunk.css"
+  },
+  {
+    "revision": "71e25d2194c4ce8aaa36",
+    "url": "/tomnium/static/js/2.f46b8b66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tomnium/static/js/2.f46b8b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df644db64623cb23243",
+    "url": "/tomnium/static/js/main.61e9b609.chunk.js"
+  },
+  {
+    "revision": "b494495bfd14eab00b41",
+    "url": "/tomnium/static/js/runtime-main.352a54e0.js"
+  }
+]);
